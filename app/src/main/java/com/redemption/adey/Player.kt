a09c2369305bb8773238dbac4b8705ac71eab26a
Natnael Mahetem <natymahetem@gmail.com>
2022-09-03 13:50:29 +0300

    done to exoplayer

--- app/src/main/java/com/redemption/adey/Player.kt
+++ app/src/main/java/com/redemption/adey/Player.kt
@@ -2,18 +2,118 @@ package com.redemption.adey
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.SparseArray
+import android.view.View
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import at.huber.youtubeExtractor.VideoMeta
+import at.huber.youtubeExtractor.YouTubeExtractor
+import at.huber.youtubeExtractor.YtFile
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.MergingMediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import com.google.android.exoplayer2.util.Util
 
 class Player : AppCompatActivity() {
+
+    private var player: SimpleExoPlayer? = null
+    private var playWhenReady = true
+    private var currentWindow = 0
+    private var playbackPosition : Long = 0
+    private lateinit var videoPlayer: com.google.android.exoplayer2.ui.PlayerView
+//    private lateinit var videoid: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
 
-        val link: TextView = findViewById(R.id.link)
+        videoPlayer = findViewById(R.id.videoPlayer)
 
-        val videoId=intent.getStringExtra("videoId")
+        initPlayer()
+    }
 
-        link.setText(videoId)
+    private fun initPlayer() {
+        player = SimpleExoPlayer.Builder(this).build()
+        videoPlayer.player = player
+
+        val videoId = intent.getStringExtra("videoId")
+
+        val videoUrl = "https://www.youtube.com/watch?v=${videoId}"
+        object : YouTubeExtractor(this){
+            override fun onExtractionComplete(
+                ytFiles: SparseArray<YtFile>?,
+                videoMeta: VideoMeta?
+            ) {
+                if(ytFiles != null) {
+                    val itag = 137
+                    val audioTag = 140
+                    val videoUrl = ytFiles[itag].url
+                    val audioUrl = ytFiles[audioTag].url
+
+                    val audioSource : MediaSource = ProgressiveMediaSource
+                        .Factory(DefaultHttpDataSource.Factory())
+                        .createMediaSource(MediaItem.fromUri(audioUrl))
+
+                    val videoSource : MediaSource = ProgressiveMediaSource
+                        .Factory(DefaultHttpDataSource.Factory())
+                        .createMediaSource(MediaItem.fromUri(videoUrl))
+
+                    player!!.setMediaSource(MergingMediaSource(
+                        true, videoSource, audioSource), true)
+                    player!!.prepare()
+                    player!!.playWhenReady = playWhenReady
+                    player!!.seekTo(currentWindow, playbackPosition)
+                }
+            }
+
+        }.extract(videoUrl, false, true)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT >= 24)
+            initPlayer()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (Util.SDK_INT < 24 || player == null) {
+            initPlayer()
+            hideSystemUi()
+        }
+    }
+
+    private fun hideSystemUi() {
+        videoPlayer.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_LOW_PROFILE or
+        View.SYSTEM_UI_FLAG_FULLSCREEN or
+        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                )
+    }
+
+    override fun onPause() {
+        if (Util.SDK_INT < 24) releasePlayer()
+        super.onPause()
+    }
+
+    override fun onStop() {
+        if (Util.SDK_INT < 24) releasePlayer()
+        super.onStop()
+    }
+
+    private fun releasePlayer() {
+        if (player != null) {
+            playWhenReady = player!!.playWhenReady
+            playbackPosition = player!!.currentPosition
+            currentWindow = player!!.currentWindowIndex
+            player!!.release()
+            player = null
+        }
     }
 }
